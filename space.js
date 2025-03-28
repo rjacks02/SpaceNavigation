3ab1f2d990d9a14bc5b2faece962c371017be1e6
Rebecca Jackson <rjacks02@villanova.edu>
2025-03-28 13:34:44 -0400

    Add moving asteroids

--- space.js
+++ space.js
@@ -11,14 +11,6 @@ let rocketX = boardWidth/20;
 let rocketY = (boardHeight - rocketHeight)/2;
 let rocketImg;
 
-//physics
-let keyPressed = false;
-let clickPressed = false;
-let touchPressed = false;
-
-let velocityY = 0;
-let gravity = .2;
-
 let rocket = {
     x : rocketX,
     y : rocketY,
@@ -26,8 +18,37 @@ let rocket = {
     height : rocketHeight
 }
 
+//asteroids
+//asteroids
+let asteroidArray = [];
+let asteroid1Width = 70;
+let asteroid1Height = 74;
+let asteroid1X = boardWidth;
+//let asteroidY = Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled);
+let asteroid1Y = 0;
+
+
+let asteroid2Width =70.4;
+let asteroid2Height = 67.2;
+let asteroid2X = boardWidth;
+//let asteroidY = Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled);
+let asteroid2Y = 0;
+
+let asteroid1Img;
+let asteroid2Img;
+
+//physics
+let keyPressed = false;
+let clickPressed = false;
+let touchPressed = false;
+
+let velocityX = -3;
+let velocityY = 0;
+let gravity = .2;
+
 //stats
-let gameOver = false
+let gameOver = false;
+let score = 0;
 
 window.onload = function() {
     //context
@@ -43,7 +64,14 @@ window.onload = function() {
         context.drawImage(rocketImg, rocket.x, rocket.y, rocket.width, rocket.height);
     }
 
+    //asteroids
+    asteroid1Img = new Image();
+    asteroid1Img.src = "./assets/asteroid1.png";
+    asteroid2Img = new Image();
+    asteroid2Img.src = "./assets/asteroid2.png";
+
     requestAnimationFrame(update);
+    setInterval(placeAsteroids, 800);
 
     document.addEventListener("keydown", function(e){
         keyPressed = true;
@@ -94,4 +122,62 @@ function update(){
 
     context.drawImage(rocketImg, rocket.x, rocket.y, rocket.width, rocket.height);
 
+
+    //asteroids
+    for (let i = 0; i < asteroidArray.length; i++){
+        let asteroid = asteroidArray[i];
+        asteroid.x += velocityX;
+        context.drawImage(asteroid.img, asteroid.x, asteroid.y, asteroid.width, asteroid.height);
+
+        if (!asteroid.passed && rocket.x > asteroid.x + asteroid.width){
+            score += .5;
+            asteroid.passed = true;
+        }
+    }
+
+
+    //clear asteroids
+    while (asteroidArray.length > 0 && asteroidArray[0].x < -asteroidWidth){
+        asteroidArray.shift();
+    }
+
+}
+
+function placeAsteroids(){
+    if (gameOver){
+        return;
+    }
+
+    let asteroidRandom = Math.random();
+
+    if (asteroidRandom < .5){
+        let randomAsteroid1Y = Math.floor(Math.random() * (boardHeight-asteroid1Height));
+        
+
+        let asteroid = {
+            img : asteroid1Img,
+            x : asteroid1X,
+            y : randomAsteroid1Y,
+            width : asteroid1Width,
+            height : asteroid1Height,
+            passed : false
+        }
+    
+        asteroidArray.push(asteroid);
+    }
+
+    else{
+        let randomAsteroid2Y = Math.floor(Math.random() * (boardHeight-asteroid2Height));
+
+        let asteroid = {
+            img : asteroid2Img,
+            x : asteroid2X,
+            y : randomAsteroid2Y,
+            width : asteroid2Width,
+            height : asteroid2Height,
+            passed : false
+        }
+    
+        asteroidArray.push(asteroid);
+    }
 }
